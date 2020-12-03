744d37235f519541b947796555bd42b5067c105f
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2020-12-03 21:05:08 +0530

    fixed cors origin

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
